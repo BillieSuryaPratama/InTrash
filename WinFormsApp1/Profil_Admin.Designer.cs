13f12f5be8dcf54d5febc5b575f710389b337758
xoxcoyyu <shmjake05@gmail.com>
2024-06-03 15:47:47 +0700

    ubah profil admin

--- WinFormsApp1/Profil_Admin.Designer.cs
+++ WinFormsApp1/Profil_Admin.Designer.cs
@@ -47,7 +47,7 @@
             // 
             // dtpTanggalLahir
             // 
-            dtpTanggalLahir.Location = new Point(231, 114);
+            dtpTanggalLahir.Location = new Point(272, 114);
             dtpTanggalLahir.Margin = new Padding(2);
             dtpTanggalLahir.Name = "dtpTanggalLahir";
             dtpTanggalLahir.Size = new Size(266, 27);
@@ -55,7 +55,7 @@
             // 
             // tbAlamat
             // 
-            tbAlamat.Location = new Point(230, 247);
+            tbAlamat.Location = new Point(271, 247);
             tbAlamat.Margin = new Padding(2);
             tbAlamat.Name = "tbAlamat";
             tbAlamat.Size = new Size(266, 27);
@@ -63,7 +63,7 @@
             // 
             // tbEmail
             // 
-            tbEmail.Location = new Point(230, 204);
+            tbEmail.Location = new Point(271, 204);
             tbEmail.Margin = new Padding(2);
             tbEmail.Name = "tbEmail";
             tbEmail.Size = new Size(266, 27);
@@ -71,7 +71,7 @@
             // 
             // tbNomorHP
             // 
-            tbNomorHP.Location = new Point(230, 157);
+            tbNomorHP.Location = new Point(271, 157);
             tbNomorHP.Margin = new Padding(2);
             tbNomorHP.Name = "tbNomorHP";
             tbNomorHP.Size = new Size(266, 27);
@@ -79,7 +79,7 @@
             // 
             // tbNama
             // 
-            tbNama.Location = new Point(230, 32);
+            tbNama.Location = new Point(271, 32);
             tbNama.Margin = new Padding(2);
             tbNama.Name = "tbNama";
             tbNama.Size = new Size(267, 27);
@@ -87,28 +87,32 @@
             // 
             // btnSimpan
             // 
+            btnSimpan.BackColor = Color.LimeGreen;
+            btnSimpan.ForeColor = Color.White;
             btnSimpan.Location = new Point(539, 322);
             btnSimpan.Margin = new Padding(2);
             btnSimpan.Name = "btnSimpan";
             btnSimpan.Size = new Size(90, 27);
             btnSimpan.TabIndex = 39;
             btnSimpan.Text = "Simpan";
-            btnSimpan.UseVisualStyleBackColor = true;
+            btnSimpan.UseVisualStyleBackColor = false;
             // 
             // btnKembali
             // 
+            btnKembali.BackColor = Color.Red;
+            btnKembali.ForeColor = Color.White;
             btnKembali.Location = new Point(11, 322);
             btnKembali.Margin = new Padding(2);
             btnKembali.Name = "btnKembali";
             btnKembali.Size = new Size(90, 27);
             btnKembali.TabIndex = 40;
             btnKembali.Text = "Kembali";
-            btnKembali.UseVisualStyleBackColor = true;
+            btnKembali.UseVisualStyleBackColor = false;
             // 
             // cbGender
             // 
             cbGender.FormattingEnabled = true;
-            cbGender.Location = new Point(230, 73);
+            cbGender.Location = new Point(271, 73);
             cbGender.Margin = new Padding(2);
             cbGender.Name = "cbGender";
             cbGender.Size = new Size(267, 28);
@@ -117,59 +121,78 @@
             // label2
             // 
             label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 10F);
+            label2.ForeColor = Color.White;
             label2.Location = new Point(123, 35);
             label2.Name = "label2";
-            label2.Size = new Size(49, 20);
+            label2.Size = new Size(56, 23);
             label2.TabIndex = 42;
             label2.Text = "Nama";
             // 
             // label3
             // 
             label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 10F);
+            label3.ForeColor = Color.White;
             label3.Location = new Point(123, 76);
             label3.Name = "label3";
-            label3.Size = new Size(57, 20);
+            label3.Size = new Size(66, 23);
             label3.TabIndex = 43;
             label3.Text = "Gender";
             // 
             // label8
             // 
             label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Segoe UI", 10F);
+            label8.ForeColor = Color.White;
             label8.Location = new Point(126, 250);
             label8.Name = "label8";
-            label8.Size = new Size(57, 20);
+            label8.Size = new Size(64, 23);
             label8.TabIndex = 48;
             label8.Text = "Alamat";
             // 
             // label6
             // 
             label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI", 10F);
+            label6.ForeColor = Color.White;
             label6.Location = new Point(123, 160);
             label6.Name = "label6";
-            label6.Size = new Size(79, 20);
+            label6.Size = new Size(91, 23);
             label6.TabIndex = 46;
             label6.Text = "Nomor HP";
             // 
             // label7
             // 
             label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI", 10F);
+            label7.ForeColor = Color.White;
             label7.Location = new Point(126, 207);
             label7.Name = "label7";
-            label7.Size = new Size(46, 20);
+            label7.Size = new Size(51, 23);
             label7.TabIndex = 47;
             label7.Text = "Email";
             // 
             // label5
             // 
             label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 10F);
+            label5.ForeColor = Color.White;
             label5.Location = new Point(123, 119);
             label5.Name = "label5";
-            label5.Size = new Size(97, 20);
+            label5.Size = new Size(111, 23);
             label5.TabIndex = 45;
             label5.Text = "Tanggal Lahir";
             // 
             // btnUbahSandi
             // 
+            btnUbahSandi.BackgroundImage = Properties.Resources.Wireframe___56;
             btnUbahSandi.Location = new Point(273, 294);
             btnUbahSandi.Name = "btnUbahSandi";
             btnUbahSandi.Size = new Size(94, 29);
@@ -181,6 +204,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Wireframe___55;
             ClientSize = new Size(640, 360);
             Controls.Add(btnUbahSandi);
             Controls.Add(label8);
