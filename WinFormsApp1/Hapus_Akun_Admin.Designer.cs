321ffe8d86a9f65c29a23e4714314a9b272f97fb
Rahmadita <01n01.leee@gmail.com>
2024-06-03 19:29:50 +0700

    sandi penghapusan akun

--- WinFormsApp1/Hapus_Akun_Admin.Designer.cs
+++ WinFormsApp1/Hapus_Akun_Admin.Designer.cs
@@ -38,56 +38,69 @@
             // lblInstruksi
             // 
             lblInstruksi.AutoSize = true;
-            lblInstruksi.Location = new Point(111, 108);
+            lblInstruksi.BackColor = Color.Transparent;
+            lblInstruksi.Font = new Font("Segoe UI", 12F);
+            lblInstruksi.ForeColor = Color.White;
+            lblInstruksi.Location = new Point(50, 50);
             lblInstruksi.Name = "lblInstruksi";
-            lblInstruksi.Size = new Size(429, 20);
+            lblInstruksi.Size = new Size(458, 21);
             lblInstruksi.TabIndex = 9;
             lblInstruksi.Text = "Masukkan sandi anda untuk mengkonfirmasi penghapusan akun";
             // 
             // btnSandi
             // 
-            btnSandi.Location = new Point(321, 169);
+            btnSandi.Location = new Point(281, 127);
+            btnSandi.Margin = new Padding(3, 2, 3, 2);
             btnSandi.Name = "btnSandi";
-            btnSandi.Size = new Size(125, 27);
+            btnSandi.Size = new Size(110, 23);
             btnSandi.TabIndex = 8;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(210, 172);
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(184, 129);
             label1.Name = "label1";
-            label1.Size = new Size(46, 20);
+            label1.Size = new Size(36, 15);
             label1.TabIndex = 7;
             label1.Text = "Sandi";
             // 
             // btnKonfirmasi
             // 
-            btnKonfirmasi.Location = new Point(533, 320);
+            btnKonfirmasi.BackgroundImage = Properties.Resources.Wireframe___56;
+            btnKonfirmasi.Location = new Point(454, 217);
+            btnKonfirmasi.Margin = new Padding(3, 2, 3, 2);
             btnKonfirmasi.Name = "btnKonfirmasi";
-            btnKonfirmasi.Size = new Size(94, 29);
+            btnKonfirmasi.Size = new Size(82, 34);
             btnKonfirmasi.TabIndex = 6;
             btnKonfirmasi.Text = "Konfirmasi";
             btnKonfirmasi.UseVisualStyleBackColor = true;
             // 
             // btnKembali
             // 
-            btnKembali.Location = new Point(12, 320);
+            btnKembali.BackColor = Color.Red;
+            btnKembali.ForeColor = Color.White;
+            btnKembali.Location = new Point(22, 217);
+            btnKembali.Margin = new Padding(3, 2, 3, 2);
             btnKembali.Name = "btnKembali";
-            btnKembali.Size = new Size(94, 29);
+            btnKembali.Size = new Size(82, 34);
             btnKembali.TabIndex = 5;
             btnKembali.Text = "Kembali";
-            btnKembali.UseVisualStyleBackColor = true;
+            btnKembali.UseVisualStyleBackColor = false;
             // 
             // Hapus_Akun_Admin
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(638, 359);
+            BackgroundImage = Properties.Resources.Wireframe___55__1_;
+            ClientSize = new Size(558, 269);
             Controls.Add(lblInstruksi);
             Controls.Add(btnSandi);
             Controls.Add(label1);
             Controls.Add(btnKonfirmasi);
             Controls.Add(btnKembali);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Hapus_Akun_Admin";
             Text = "InTrash";
             ResumeLayout(false);
