affc445b1caa7b52d1413a5622676316d5849979
Qurrotu Aini <qurrotuaini1357@gmail.com>
2024-06-03 20:41:56 +0700

    Ubah Verif No Hp

--- WinFormsApp1/Verifikasi_NoHP.Designer.cs
+++ WinFormsApp1/Verifikasi_NoHP.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Verifikasi_NoHP));
             btnBerikutnya = new Button();
             tbNomorHP = new TextBox();
             lblInstruksi = new Label();
@@ -37,7 +38,7 @@
             // 
             // btnBerikutnya
             // 
-            btnBerikutnya.Location = new Point(655, 388);
+            btnBerikutnya.Location = new Point(655, 366);
             btnBerikutnya.Margin = new Padding(2);
             btnBerikutnya.Name = "btnBerikutnya";
             btnBerikutnya.Size = new Size(112, 34);
@@ -47,26 +48,31 @@
             // 
             // tbNomorHP
             // 
-            tbNomorHP.Location = new Point(370, 160);
+            tbNomorHP.Location = new Point(286, 246);
             tbNomorHP.Margin = new Padding(2);
             tbNomorHP.Name = "tbNomorHP";
-            tbNomorHP.Size = new Size(150, 31);
+            tbNomorHP.Size = new Size(215, 31);
             tbNomorHP.TabIndex = 1;
             // 
             // lblInstruksi
             // 
             lblInstruksi.AutoSize = true;
-            lblInstruksi.Location = new Point(172, 105);
+            lblInstruksi.BackColor = Color.Transparent;
+            lblInstruksi.Font = new Font("Perpetua", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblInstruksi.ForeColor = SystemColors.ButtonHighlight;
+            lblInstruksi.Location = new Point(173, 142);
             lblInstruksi.Margin = new Padding(4, 0, 4, 0);
             lblInstruksi.Name = "lblInstruksi";
-            lblInstruksi.Size = new Size(421, 25);
+            lblInstruksi.Size = new Size(447, 28);
             lblInstruksi.TabIndex = 2;
-            lblInstruksi.Text = "Masukkan nomor HP anda sebagai tahap verikfikasi";
+            lblInstruksi.Text = "Masukkan nomor HP anda sebagai tahap verifikasi";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(250, 164);
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.Transparent;
+            label2.Location = new Point(343, 201);
             label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
             label2.Size = new Size(97, 25);
@@ -75,7 +81,7 @@
             // 
             // btnKembali
             // 
-            btnKembali.Location = new Point(30, 388);
+            btnKembali.Location = new Point(28, 366);
             btnKembali.Margin = new Padding(2);
             btnKembali.Name = "btnKembali";
             btnKembali.Size = new Size(112, 34);
@@ -88,6 +94,7 @@
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Green;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(800, 450);
             Controls.Add(btnKembali);
             Controls.Add(label2);
