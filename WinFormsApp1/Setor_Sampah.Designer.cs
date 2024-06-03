8aef3e607ba07acb0849ab727cdf9eeb8028d460
Qurrotu Aini <qurrotuaini1357@gmail.com>
2024-06-03 20:58:58 +0700

    Ubah Setor Sampah

--- WinFormsApp1/Setor_Sampah.Designer.cs
+++ WinFormsApp1/Setor_Sampah.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Setor_Sampah));
             textBox2 = new TextBox();
             textBox3 = new TextBox();
             comboBox1 = new ComboBox();
@@ -45,119 +46,141 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(255, 51);
+            textBox2.Location = new Point(319, 64);
             textBox2.Margin = new Padding(2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(261, 27);
+            textBox2.Size = new Size(325, 31);
             textBox2.TabIndex = 18;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(255, 81);
+            textBox3.Location = new Point(319, 101);
             textBox3.Margin = new Padding(2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(261, 27);
+            textBox3.Size = new Size(325, 31);
             textBox3.TabIndex = 19;
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(255, 112);
+            comboBox1.Location = new Point(319, 140);
             comboBox1.Margin = new Padding(2);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(261, 28);
+            comboBox1.Size = new Size(325, 33);
             comboBox1.TabIndex = 33;
             comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(255, 22);
+            dateTimePicker1.Location = new Point(319, 28);
             dateTimePicker1.Margin = new Padding(2);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(261, 27);
+            dateTimePicker1.Size = new Size(325, 31);
             dateTimePicker1.TabIndex = 37;
             // 
             // btnBerikutnya
             // 
-            btnBerikutnya.Location = new Point(539, 322);
+            btnBerikutnya.Location = new Point(674, 402);
             btnBerikutnya.Margin = new Padding(2);
             btnBerikutnya.Name = "btnBerikutnya";
-            btnBerikutnya.Size = new Size(90, 27);
+            btnBerikutnya.Size = new Size(112, 34);
             btnBerikutnya.TabIndex = 40;
             btnBerikutnya.Text = "Berikutnya";
             btnBerikutnya.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            button4.Location = new Point(382, 154);
+            button4.BackColor = Color.LightSeaGreen;
+            button4.Font = new Font("Palatino Linotype", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.Transparent;
+            button4.Location = new Point(478, 192);
             button4.Margin = new Padding(2);
             button4.Name = "button4";
-            button4.Size = new Size(134, 27);
+            button4.Size = new Size(168, 34);
             button4.TabIndex = 42;
             button4.Text = "Tambah Sampah";
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
             // 
             // dataGridView1
             // 
+            dataGridView1.BackgroundColor = Color.CadetBlue;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(122, 185);
+            dataGridView1.Location = new Point(154, 254);
             dataGridView1.Margin = new Padding(2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(394, 99);
+            dataGridView1.Size = new Size(492, 124);
             dataGridView1.TabIndex = 43;
             // 
             // btnKembali
             // 
-            btnKembali.Location = new Point(11, 322);
+            btnKembali.BackColor = Color.PaleTurquoise;
+            btnKembali.Location = new Point(14, 402);
             btnKembali.Margin = new Padding(2);
             btnKembali.Name = "btnKembali";
-            btnKembali.Size = new Size(90, 27);
+            btnKembali.Size = new Size(112, 34);
             btnKembali.TabIndex = 44;
             btnKembali.Text = "Kembali";
-            btnKembali.UseVisualStyleBackColor = true;
+            btnKembali.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(122, 27);
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Palatino Linotype", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Transparent;
+            label2.Location = new Point(152, 28);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(61, 20);
+            label2.Size = new Size(84, 27);
             label2.TabIndex = 45;
             label2.Text = "Tanggal";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(122, 54);
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Palatino Linotype", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Transparent;
+            label3.Location = new Point(152, 68);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(57, 20);
+            label3.Size = new Size(79, 27);
             label3.TabIndex = 46;
             label3.Text = "Alamat";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(122, 84);
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Palatino Linotype", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Transparent;
+            label4.Location = new Point(152, 105);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(69, 20);
+            label4.Size = new Size(101, 27);
             label4.TabIndex = 47;
             label4.Text = "Deskripsi";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(122, 115);
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Palatino Linotype", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Transparent;
+            label1.Location = new Point(152, 144);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(128, 20);
+            label1.Size = new Size(162, 24);
             label1.TabIndex = 48;
             label1.Text = "Metode Pencairan";
             // 
             // Setor_Sampah
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(640, 360);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(800, 450);
             Controls.Add(label1);
             Controls.Add(label4);
             Controls.Add(label3);
