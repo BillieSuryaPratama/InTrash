174f51861b4c60579fa8bb52852791343ddd8280
Dylla putri ananda <dyllaputri59@gmail.com>
2024-06-03 20:13:44 +0700

    dylla_konfirmasi jemput sampah

--- WinFormsApp1/Konfirmasi_Jemput_Sampah.Designer.cs
+++ WinFormsApp1/Konfirmasi_Jemput_Sampah.Designer.cs
@@ -48,65 +48,70 @@
             // lblNama
             // 
             lblNama.AutoSize = true;
-            lblNama.Location = new Point(219, 14);
+            lblNama.Location = new Point(192, 10);
             lblNama.Name = "lblNama";
-            lblNama.Size = new Size(59, 20);
+            lblNama.Size = new Size(49, 15);
             lblNama.TabIndex = 64;
             lblNama.Text = "dummy";
             // 
             // lblAlamat
             // 
             lblAlamat.AutoSize = true;
-            lblAlamat.Location = new Point(219, 43);
+            lblAlamat.Location = new Point(192, 32);
             lblAlamat.Name = "lblAlamat";
-            lblAlamat.Size = new Size(59, 20);
+            lblAlamat.Size = new Size(49, 15);
             lblAlamat.TabIndex = 63;
             lblAlamat.Text = "dummy";
             // 
             // lblDeskripsi
             // 
             lblDeskripsi.AutoSize = true;
-            lblDeskripsi.Location = new Point(219, 74);
+            lblDeskripsi.Location = new Point(192, 56);
             lblDeskripsi.Name = "lblDeskripsi";
-            lblDeskripsi.Size = new Size(59, 20);
+            lblDeskripsi.Size = new Size(49, 15);
             lblDeskripsi.TabIndex = 62;
             lblDeskripsi.Text = "dummy";
             // 
             // lblMetodePencairan
             // 
             lblMetodePencairan.AutoSize = true;
-            lblMetodePencairan.Location = new Point(219, 107);
+            lblMetodePencairan.Location = new Point(192, 80);
             lblMetodePencairan.Name = "lblMetodePencairan";
-            lblMetodePencairan.Size = new Size(59, 20);
+            lblMetodePencairan.Size = new Size(49, 15);
             lblMetodePencairan.TabIndex = 61;
             lblMetodePencairan.Text = "dummy";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(59, 146);
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(52, 110);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(89, 20);
+            label2.Size = new Size(71, 15);
             label2.TabIndex = 60;
             label2.Text = "List Sampah";
             // 
             // btnKembali
             // 
-            btnKembali.Location = new Point(11, 319);
+            btnKembali.BackColor = Color.Red;
+            btnKembali.ForeColor = Color.White;
+            btnKembali.Location = new Point(20, 235);
             btnKembali.Margin = new Padding(2);
             btnKembali.Name = "btnKembali";
-            btnKembali.Size = new Size(90, 27);
+            btnKembali.Size = new Size(79, 24);
             btnKembali.TabIndex = 53;
             btnKembali.Text = "Kembali";
-            btnKembali.UseVisualStyleBackColor = true;
+            btnKembali.UseVisualStyleBackColor = false;
             // 
             // btnKonfirmasi
             // 
-            btnKonfirmasi.Location = new Point(541, 318);
+            btnKonfirmasi.BackgroundImage = Properties.Resources.Wireframe___56;
+            btnKonfirmasi.Location = new Point(463, 235);
             btnKonfirmasi.Margin = new Padding(2);
             btnKonfirmasi.Name = "btnKonfirmasi";
-            btnKonfirmasi.Size = new Size(89, 27);
+            btnKonfirmasi.Size = new Size(78, 23);
             btnKonfirmasi.TabIndex = 59;
             btnKonfirmasi.Text = "Konfirmasi";
             btnKonfirmasi.UseVisualStyleBackColor = true;
@@ -114,67 +119,77 @@
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(59, 74);
+            label10.BackColor = Color.Transparent;
+            label10.ForeColor = Color.White;
+            label10.Location = new Point(52, 56);
             label10.Margin = new Padding(2, 0, 2, 0);
             label10.Name = "label10";
-            label10.Size = new Size(69, 20);
+            label10.Size = new Size(54, 15);
             label10.TabIndex = 58;
             label10.Text = "Deskripsi";
             // 
             // dgvListSampah
             // 
+            dgvListSampah.BackgroundColor = Color.Gainsboro;
             dgvListSampah.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvListSampah.Location = new Point(59, 168);
+            dgvListSampah.Location = new Point(52, 131);
             dgvListSampah.Margin = new Padding(2);
             dgvListSampah.Name = "dgvListSampah";
             dgvListSampah.RowHeadersWidth = 62;
-            dgvListSampah.Size = new Size(530, 120);
+            dgvListSampah.Size = new Size(464, 90);
             dgvListSampah.TabIndex = 57;
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(59, 107);
+            label8.BackColor = Color.Transparent;
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(52, 80);
             label8.Margin = new Padding(2, 0, 2, 0);
             label8.Name = "label8";
-            label8.Size = new Size(128, 20);
+            label8.Size = new Size(103, 15);
             label8.TabIndex = 56;
             label8.Text = "Metode Pencairan";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(59, 43);
+            label6.BackColor = Color.Transparent;
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(52, 32);
             label6.Margin = new Padding(2, 0, 2, 0);
             label6.Name = "label6";
-            label6.Size = new Size(57, 20);
+            label6.Size = new Size(45, 15);
             label6.TabIndex = 55;
             label6.Text = "Alamat";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(219, 43);
+            label4.Location = new Point(192, 32);
             label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(0, 20);
+            label4.Size = new Size(0, 15);
             label4.TabIndex = 54;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(59, 14);
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(52, 10);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(49, 20);
+            label1.Size = new Size(39, 15);
             label1.TabIndex = 52;
             label1.Text = "Nama";
             // 
             // Konfirmasi_Jemput_Sampah
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(640, 360);
+            BackgroundImage = Properties.Resources.Wireframe___55;
+            ClientSize = new Size(560, 270);
             Controls.Add(lblNama);
             Controls.Add(lblAlamat);
             Controls.Add(lblDeskripsi);
@@ -188,7 +203,7 @@
             Controls.Add(label6);
             Controls.Add(label4);
             Controls.Add(label1);
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "Konfirmasi_Jemput_Sampah";
             Text = "InTrash";
             ((System.ComponentModel.ISupportInitialize)dgvListSampah).EndInit();
