9a51aa3071e89cba5eec99015cfdd7ad1bfe61af
Rahmadita <01n01.leee@gmail.com>
2024-06-03 17:30:08 +0700

    Rahmadita_jenis sampah

--- WinFormsApp1/Atur_Jenis_Sampah.Designer.cs
+++ WinFormsApp1/Atur_Jenis_Sampah.Designer.cs
@@ -40,53 +40,56 @@
             // 
             btnKembali.BackColor = Color.Red;
             btnKembali.ForeColor = Color.White;
-            btnKembali.Location = new Point(11, 310);
+            btnKembali.Location = new Point(24, 220);
             btnKembali.Margin = new Padding(2);
             btnKembali.Name = "btnKembali";
-            btnKembali.Size = new Size(90, 27);
+            btnKembali.Size = new Size(79, 27);
             btnKembali.TabIndex = 49;
             btnKembali.Text = "Kembali";
             btnKembali.UseVisualStyleBackColor = false;
             // 
             // tbEdit
             // 
-            tbEdit.Location = new Point(314, 258);
+            tbEdit.Location = new Point(275, 194);
             tbEdit.Margin = new Padding(2);
             tbEdit.Name = "tbEdit";
-            tbEdit.Size = new Size(166, 27);
+            tbEdit.Size = new Size(140, 23);
             tbEdit.TabIndex = 48;
             // 
             // btnEditTambah
             // 
+            btnEditTambah.BackColor = Color.Black;
             btnEditTambah.BackgroundImage = (Image)resources.GetObject("btnEditTambah.BackgroundImage");
-            btnEditTambah.Location = new Point(493, 258);
+            btnEditTambah.Location = new Point(431, 194);
             btnEditTambah.Margin = new Padding(2);
             btnEditTambah.Name = "btnEditTambah";
-            btnEditTambah.Size = new Size(122, 27);
+            btnEditTambah.Size = new Size(107, 23);
             btnEditTambah.TabIndex = 47;
             btnEditTambah.Text = "Edit / Tambah";
-            btnEditTambah.UseVisualStyleBackColor = true;
+            btnEditTambah.UseVisualStyleBackColor = false;
             // 
             // dgvListJenisSampah
             // 
+            dgvListJenisSampah.BackgroundColor = Color.Gainsboro;
             dgvListJenisSampah.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvListJenisSampah.Location = new Point(27, 11);
+            dgvListJenisSampah.Location = new Point(24, 8);
             dgvListJenisSampah.Margin = new Padding(2);
             dgvListJenisSampah.Name = "dgvListJenisSampah";
             dgvListJenisSampah.RowHeadersWidth = 62;
-            dgvListJenisSampah.Size = new Size(588, 225);
+            dgvListJenisSampah.Size = new Size(514, 169);
             dgvListJenisSampah.TabIndex = 46;
             // 
             // Atur_Jenis_Sampah
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(637, 348);
+            BackgroundImage = Properties.Resources.Wireframe___55__1_;
+            ClientSize = new Size(557, 261);
             Controls.Add(btnKembali);
             Controls.Add(tbEdit);
             Controls.Add(btnEditTambah);
             Controls.Add(dgvListJenisSampah);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Atur_Jenis_Sampah";
             Text = "InTrash";
             ((System.ComponentModel.ISupportInitialize)dgvListJenisSampah).EndInit();
