ed747b4946e93b1b7aba6de7b7020725b4c528ad
BillieSuryaPratama <billie.surya.pratama@gmail.com>
2024-06-11 08:34:20 +0700

    Register Kurir (DONE)

--- WinFormsApp1/Views/Register_Kurir.Designer.cs
+++ WinFormsApp1/Views/Register_Kurir.Designer.cs
@@ -39,6 +39,14 @@
             label3 = new Label();
             label4 = new Label();
             lblInstruksi = new Label();
+            label5 = new Label();
+            cbTempatPengepul = new ComboBox();
+            dgvTempatPengepul = new DataGridView();
+            Id_tempatpengepul = new DataGridViewTextBoxColumn();
+            nama_tempat = new DataGridViewTextBoxColumn();
+            Alamat = new DataGridViewTextBoxColumn();
+            Deskripsi = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvTempatPengepul).BeginInit();
             SuspendLayout();
             // 
             // btnKembali
@@ -46,7 +54,7 @@
             btnKembali.BackColor = Color.Red;
             btnKembali.Font = new Font("Segoe UI", 9F);
             btnKembali.ForeColor = Color.White;
-            btnKembali.Location = new Point(14, 322);
+            btnKembali.Location = new Point(17, 385);
             btnKembali.Margin = new Padding(2);
             btnKembali.Name = "btnKembali";
             btnKembali.Size = new Size(90, 27);
@@ -60,48 +68,49 @@
             btnSimpan.BackColor = Color.LimeGreen;
             btnSimpan.BackgroundImage = Properties.Resources.Wireframe___56;
             btnSimpan.ForeColor = Color.Black;
-            btnSimpan.Location = new Point(532, 320);
+            btnSimpan.Location = new Point(535, 383);
             btnSimpan.Margin = new Padding(2);
             btnSimpan.Name = "btnSimpan";
             btnSimpan.Size = new Size(90, 27);
             btnSimpan.TabIndex = 42;
             btnSimpan.Text = "Simpan";
             btnSimpan.UseVisualStyleBackColor = false;
+            btnSimpan.Click += btnSimpan_Click;
             // 
             // tbSandi
             // 
             tbSandi.Font = new Font("Segoe UI", 10F);
-            tbSandi.Location = new Point(313, 216);
+            tbSandi.Location = new Point(291, 136);
             tbSandi.Margin = new Padding(2);
             tbSandi.Name = "tbSandi";
-            tbSandi.Size = new Size(121, 30);
+            tbSandi.Size = new Size(224, 30);
             tbSandi.TabIndex = 40;
             // 
             // tbUsername
             // 
             tbUsername.Font = new Font("Segoe UI", 10F);
-            tbUsername.Location = new Point(313, 187);
+            tbUsername.Location = new Point(291, 107);
             tbUsername.Margin = new Padding(2);
             tbUsername.Name = "tbUsername";
-            tbUsername.Size = new Size(121, 30);
+            tbUsername.Size = new Size(224, 30);
             tbUsername.TabIndex = 43;
             // 
             // tbNomorHP
             // 
             tbNomorHP.Font = new Font("Segoe UI", 10F);
-            tbNomorHP.Location = new Point(313, 157);
+            tbNomorHP.Location = new Point(291, 77);
             tbNomorHP.Margin = new Padding(2);
             tbNomorHP.Name = "tbNomorHP";
-            tbNomorHP.Size = new Size(121, 30);
+            tbNomorHP.Size = new Size(224, 30);
             tbNomorHP.TabIndex = 44;
             // 
             // tbNama
             // 
             tbNama.Font = new Font("Segoe UI", 10F);
-            tbNama.Location = new Point(313, 127);
+            tbNama.Location = new Point(291, 47);
             tbNama.Margin = new Padding(2);
             tbNama.Name = "tbNama";
-            tbNama.Size = new Size(121, 30);
+            tbNama.Size = new Size(224, 30);
             tbNama.TabIndex = 45;
             // 
             // label1
@@ -110,7 +119,7 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 10F);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(189, 129);
+            label1.Location = new Point(134, 49);
             label1.Name = "label1";
             label1.Size = new Size(56, 23);
             label1.TabIndex = 46;
@@ -122,7 +131,7 @@
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Segoe UI", 10F);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(189, 160);
+            label2.Location = new Point(134, 80);
             label2.Name = "label2";
             label2.Size = new Size(91, 23);
             label2.TabIndex = 47;
@@ -134,7 +143,7 @@
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Segoe UI", 10F);
             label3.ForeColor = Color.White;
-            label3.Location = new Point(189, 190);
+            label3.Location = new Point(134, 110);
             label3.Name = "label3";
             label3.Size = new Size(87, 23);
             label3.TabIndex = 48;
@@ -146,7 +155,7 @@
             label4.BackColor = Color.Transparent;
             label4.Font = new Font("Segoe UI", 10F);
             label4.ForeColor = Color.White;
-            label4.Location = new Point(189, 219);
+            label4.Location = new Point(134, 139);
             label4.Name = "label4";
             label4.Size = new Size(52, 23);
             label4.TabIndex = 49;
@@ -158,11 +167,69 @@
             lblInstruksi.BackColor = Color.Transparent;
             lblInstruksi.Font = new Font("Segoe UI", 14F);
             lblInstruksi.ForeColor = Color.White;
-            lblInstruksi.Location = new Point(236, 54);
+            lblInstruksi.Location = new Point(231, 5);
             lblInstruksi.Name = "lblInstruksi";
             lblInstruksi.Size = new Size(172, 32);
             lblInstruksi.TabIndex = 52;
             lblInstruksi.Text = "Daftarkan kurir";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 10F);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(134, 173);
+            label5.Name = "label5";
+            label5.Size = new Size(142, 23);
+            label5.TabIndex = 54;
+            label5.Text = "Tempat Pengepul";
+            // 
+            // cbTempatPengepul
+            // 
+            cbTempatPengepul.FormattingEnabled = true;
+            cbTempatPengepul.Location = new Point(291, 168);
+            cbTempatPengepul.Name = "cbTempatPengepul";
+            cbTempatPengepul.Size = new Size(224, 28);
+            cbTempatPengepul.TabIndex = 55;
+            // 
+            // dgvTempatPengepul
+            // 
+            dgvTempatPengepul.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvTempatPengepul.Columns.AddRange(new DataGridViewColumn[] { Id_tempatpengepul, nama_tempat, Alamat, Deskripsi });
+            dgvTempatPengepul.Location = new Point(14, 215);
+            dgvTempatPengepul.Name = "dgvTempatPengepul";
+            dgvTempatPengepul.RowHeadersWidth = 51;
+            dgvTempatPengepul.Size = new Size(611, 163);
+            dgvTempatPengepul.TabIndex = 56;
+            // 
+            // Id_tempatpengepul
+            // 
+            Id_tempatpengepul.HeaderText = "ID";
+            Id_tempatpengepul.MinimumWidth = 6;
+            Id_tempatpengepul.Name = "Id_tempatpengepul";
+            Id_tempatpengepul.Width = 40;
+            // 
+            // nama_tempat
+            // 
+            nama_tempat.HeaderText = "Nama Tempat Pengepul";
+            nama_tempat.MinimumWidth = 6;
+            nama_tempat.Name = "nama_tempat";
+            nama_tempat.Width = 200;
+            // 
+            // Alamat
+            // 
+            Alamat.HeaderText = "Alamat";
+            Alamat.MinimumWidth = 6;
+            Alamat.Name = "Alamat";
+            Alamat.Width = 200;
+            // 
+            // Deskripsi
+            // 
+            Deskripsi.HeaderText = "Deskripsi";
+            Deskripsi.MinimumWidth = 6;
+            Deskripsi.Name = "Deskripsi";
+            Deskripsi.Width = 118;
             // 
             // Register_Kurir
             // 
@@ -170,7 +237,10 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Teal;
             BackgroundImage = Properties.Resources.Wireframe___55;
-            ClientSize = new Size(637, 363);
+            ClientSize = new Size(637, 430);
+            Controls.Add(dgvTempatPengepul);
+            Controls.Add(cbTempatPengepul);
+            Controls.Add(label5);
             Controls.Add(lblInstruksi);
             Controls.Add(btnKembali);
             Controls.Add(label4);
@@ -184,6 +254,8 @@
             Controls.Add(tbSandi);
             Name = "Register_Kurir";
             Text = "InTrash";
+            Load += Register_Kurir_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvTempatPengepul).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -200,5 +272,12 @@
         private Label label3;
         private Label label4;
         private Label lblInstruksi;
+        private Label label5;
+        private ComboBox cbTempatPengepul;
+        private DataGridView dgvTempatPengepul;
+        private DataGridViewTextBoxColumn Id_tempatpengepul;
+        private DataGridViewTextBoxColumn nama_tempat;
+        private DataGridViewTextBoxColumn Alamat;
+        private DataGridViewTextBoxColumn Deskripsi;
     }
 }
--- WinFormsApp1/Views/Register_Kurir.cs
+++ WinFormsApp1/Views/Register_Kurir.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
+using WinFormsApp1.Controllers;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace WinFormsApp1.Views
 {
@@ -23,6 +28,122 @@ namespace WinFormsApp1.Views
             Beranda_Admin nextpage = new Beranda_Admin();
             nextpage.FormClosed += (s, args) => this.Close();
             nextpage.ShowDialog();
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DBConnection.openConn();
+
+                string query = @"INSERT INTO Kurir (Nama_Kurir, Username_Kurir, Password_Kurir, No_HP, Id_TempatPengepul)
+                                 VALUES (@nama, @username, @password, @nohp, @id_tempatpengepul)";
+
+                using (var cmd = new NpgsqlCommand(query, DBConnection.connection))
+                {
+                    cmd.Parameters.AddWithValue("nama", tbNama.Text);
+                    cmd.Parameters.AddWithValue("nohp", tbNomorHP.Text);
+                    cmd.Parameters.AddWithValue("username", tbUsername.Text);
+                    cmd.Parameters.AddWithValue("password", tbSandi.Text);
+                    cmd.Parameters.AddWithValue("id_tempatpengepul", (int)cbTempatPengepul.SelectedValue);
+
+                    int result = cmd.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Data berhasil ditambahkan!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Gagal menambahkan data!", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                DBConnection.closeConn();
+                this.Hide();
+                Beranda_Admin nextpage = new Beranda_Admin();
+                nextpage.FormClosed += (s, args) => this.Close();
+                nextpage.ShowDialog();
+            }
+        }
+
+        private void Register_Kurir_Load(object sender, EventArgs e)
+        {
+            dgvTempatPengepul.AutoGenerateColumns = false;
+
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("ID Tempat Pengepul", typeof(int));
+            dataTable.Columns.Add("Nama Tempat", typeof(string));
+            dataTable.Columns.Add("Alamat", typeof(string));
+            dataTable.Columns.Add("Deskripsi", typeof(string));
+
+            try
+            {
+                DBConnection.openConn();
+                string query = @"SELECT id_tempatpengepul, nama_tempat, alamat, deskripsi 
+                                 FROM TempatPengepul";
+                using (var cmd = new NpgsqlCommand(query, DBConnection.connection))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        dataTable.Rows.Add(reader["id_tempatpengepul"], reader["nama_tempat"], reader["alamat"], reader["deskripsi"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}\n\nDetail: {ex.StackTrace}", "Error Database", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                DBConnection.closeConn();
+            }
+
+            dgvTempatPengepul.DataSource = dataTable;
+
+            dgvTempatPengepul.Columns[0].DataPropertyName = "ID Tempat Pengepul";
+            dgvTempatPengepul.Columns[1].DataPropertyName = "Nama Tempat";
+            dgvTempatPengepul.Columns[2].DataPropertyName = "Alamat";
+            dgvTempatPengepul.Columns[3].DataPropertyName = "Deskripsi";
+
+
+            try
+            {
+                DBConnection.openConn();
+                string query = @"SELECT id_tempatpengepul, nama_tempat
+                                 FROM TempatPengepul";
+
+                using (var cmd = new NpgsqlCommand(query, DBConnection.connection))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    var dataSource = new List<KeyValuePair<int, string>>();
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string name = reader.GetString(1);
+                        dataSource.Add(new KeyValuePair<int, string>(id, name));
+                    }
+
+                    cbTempatPengepul.DataSource = dataSource;
+                    cbTempatPengepul.ValueMember = "Key";
+                    cbTempatPengepul.DisplayMember = "Value";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}\n\nDetail: {ex.StackTrace}", "Error Database", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                DBConnection.closeConn();
+            }
         }
     }
 }
