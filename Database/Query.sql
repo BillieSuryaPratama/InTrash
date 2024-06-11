5d1b29f14240024090818b6674753892b2e109f8
BillieSuryaPratama <billie.surya.pratama@gmail.com>
2024-06-11 21:24:05 +0700

    Update Database

--- Database/Query.sql
+++ Database/Query.sql
@@ -42,13 +42,19 @@ CREATE TABLE MetodePencairan (
 	Id_Metode serial PRIMARY KEY,
 	Nama_Metode varchar not null);
 	
+CREATE TABLE Status_Transaksi (
+	Id_StatusTransaksi serial PRIMARY KEY,
+	Status varchar not null,
+	Keterangan varchar not null);
+	
 CREATE TABLE Transaksi (
 	Id_Transaksi serial PRIMARY KEY,
 	Tanggal_Transaksi date not null,
+	Jumlah_Imbalan int,
 	Id_tempatpengepul int,
 	Id_Admin int,
 	Id_Customer int,
-	Id_Metode int);
+	Id_StatusTransaksi int);
 alter table transaksi
 add constraint fk_admin_tempat_pengepul
 foreign key (Id_Admin)
@@ -62,9 +68,9 @@ add constraint fk_Customer
 foreign key (Id_Customer)
 references Customer(Id_Customer);
 alter table transaksi
-add constraint fk_MetodePencairan
-foreign key (Id_Metode)
-references MetodePencairan(Id_Metode);
+add constraint fk_Status_Transaksi
+foreign key (Id_StatusTransaksi)
+references Status_Transaksi(Id_StatusTransaksi);
 
 CREATE TABLE Jenis_Sampah (
 	Id_Jenis_Sampah serial PRIMARY KEY,
@@ -75,7 +81,7 @@ CREATE TABLE Jenis_Sampah (
 CREATE TABLE Detail_Transaksi (
 	Id_Transaksi int,
 	Id_Jenis_Sampah int,
-	Kuantitas int not null,
+	Kuantitas numeric not null,
 	constraint fk_Transaksi
 	foreign key (Id_Transaksi)
 	references Transaksi(Id_Transaksi),
@@ -84,35 +90,95 @@ CREATE TABLE Detail_Transaksi (
 	references Jenis_Sampah(Id_Jenis_Sampah)
 );
 
+CREATE TABLE Status_Pencairan (
+	Id_StatusPencairan serial PRIMARY KEY,
+	Status varchar not null,
+	Keterangan varchar not null);
+	
+CREATE TABLE Pencairan (
+	ID_Pencairan Serial PRIMARY KEY,
+	Rekening_Tujuan varchar,
+	Nominal_Pencairan int,
+	Id_StatusPencairan int,
+	Id_Metode int,
+	Id_Customer int,
+	constraint fk_Customer
+	foreign key (Id_Customer)
+	references Customer(Id_Customer),
+	constraint fk_MetodePencairan
+	foreign key (Id_Metode)
+	references MetodePencairan(Id_Metode),
+	constraint fk_Status_Pencairan
+	foreign key (Id_StatusPencairan)
+	references Status_Pencairan(Id_StatusPencairan)
+);
+
+INSERT INTO Status_Pencairan(Status, Keterangan)
+VALUES('Diproses', 'Admin belum melakukan konfirmasi pencairan'),
+	  ('Selesai', 'Admin telah melakukan konfirmasi pencairan'),
+	  ('Dibatalkan', 'Pencairan dibatalkan');
+	
+INSERT INTO Status_Transaksi(Status, Keterangan)
+VALUES('Diproses', 'Admin belum melakukan konfirmasi terhadap setoran sampah'),
+	  ('Terkonfirmasi', 'Admin telah melakukan konfirmasi terhadap setoran sampah'),
+	  ('Selesai', 'Kurir telah mengirimkan sampah ke bank sampah'),
+	  ('Dibatalkan', 'Transaksi setor sampah dibatalkan');
+
 INSERT INTO tempatpengepul(Nama_tempat, Alamat, Deskripsi)
-VALUES('Bank Sampah Sahabat Ibu', 'Jl. Perumahan Taman Gading, Kaliwates, Jember','');
+VALUES('Bank Sampah Sahabat Ibu', 'Jl. Perumahan Taman Gading, Kaliwates, Jember',''),
+	  ('Bank Sampah Peduli Lingkungan', 'Jl. Raya Sumberbaru, Sumberbaru, Jember', ''),
+	  ('Bank Sampah Hijau Lestari', 'Jl. Riau No.5, Sumbersari, Jember', '');
 
 INSERT INTO customer(Nama_Customer, Username_Customer, Password_Customer, No_HP, Alamat, Saldo)
-VALUES ('Fajar Danuarta','FajarUye','Fajar123','082229336987','Jl. Kalimantan V, Sumbersari, Jember', 0);
+VALUES ('Fajar Danuarta','FajarUye','Fajar123','082229336987','Jl. Kalimantan V, Sumbersari, Jember', 0),
+       ('Siti Aminah', 'Siti', 'Siti123', '081234567890', 'Jl. Merak No.10, Patrang, Jember', 0),
+       ('Budi Santoso', 'BudSan', 'Budi123', '085678901234', 'Jl. Jawa No.20, Kaliwates, Jember', 0);
 
 INSERT INTO admin_tempat_pengepul(Nama_admin, Username_Admin, Password_Admin, Alamat, No_HP, Id_tempatpengepul)
-VALUES ('Adrianno','Adrianno12','Adrianno157','Jl. Kalimantan XVI, Sumbersari, Jember', '081332456777', 1);
+VALUES ('Adrianno','Adrianno12','Adrianno157','Jl. Kalimantan XVI, Sumbersari, Jember', '081332456777', 1),
+  	   ('Rina Andriani', 'RinaAndri', 'Rina456', 'Jl. Jawa VIII, Kaliwates, Jember', '082145678901', 2),
+       ('Shodiqin', 'sodiq', 'soqin', 'Jl. Sumatera II, Patrang, Jember', '083156789012', 3);
 
 INSERT INTO kurir(Nama_Kurir,Username_Kurir,Password_Kurir,No_HP,Id_tempatpengepul)
-VALUES ('Jefri Nichol','JefriFriendly','Jefri123','085258909788',1);
+VALUES ('Jefri Nichol','JefriFriendly','Jefri123','085258909788',1),
+	   ('Andi Wijaya', 'Andiw', 'Andi456', '081334567890', 2),
+       ('Dewi Sartika', 'Deewi', 'Dewi789', '082145678901', 3);
 
 INSERT INTO MetodePencairan (Nama_Metode)
 VALUES ('Cash'),
-	   ('Saldo');
+	   ('Debit');
 
-INSERT INTO transaksi (tanggal_transaksi, id_tempatpengepul,id_customer, id_admin, id_metode)
-VALUES('2024-04-23', 1,1,1,1),
-      ('2024-06-10', 1,1,1,2);
+INSERT INTO transaksi (tanggal_transaksi, id_tempatpengepul, id_customer, id_admin, jumlah_imbalan, Id_StatusTransaksi)
+VALUES ('2024-02-18', 1, 2, 1, 6000, 3),
+	   ('2024-03-25', 1, 3, 1, 13000, 3),
+	   ('2024-04-20', 3, 1, 3, 3900, 3),
+	   ('2024-05-05', 2, 2, 2, 6000, 3),
+	   ('2024-05-15', 2, 1, 2, 8000, 3),
+	   ('2024-06-10', 1, 3, 1, 6900, 3);
 
 INSERT INTO jenis_sampah (Nama_Jenis_Sampah, Harga, Deskripsi)
-VALUES('Kaca',1200,'Sampah berbasis kaca, contohnya : Botol Minuman'),
-	  ('Plastik',2000,'Sampah berbasis plastik, contohnya : Botol Plastik, Kantong Plastik, Kemasan produk');
+VALUES('Kaca', 1200, 'Sampah berbasis kaca, contohnya: Botol Minuman'),
+      ('Plastik', 2000, 'Sampah berbasis plastik, contohnya: Botol Plastik, Kantong Plastik, Kemasan produk'),
+      ('Kertas', 1500, 'Sampah berbasis kertas, contohnya: Kardus, Kertas HVS, Majalah'),
+      ('Logam', 3000, 'Sampah berbasis logam, contohnya: Kaleng, Tutup Botol');
 
 INSERT INTO Detail_Transaksi (id_transaksi, id_jenis_sampah, kuantitas)
-VALUES (1,1,3),
-       (1,2,10),
-	   (2,1,2),
-	   (2,2,1);
+VALUES (1, 3, 2), 
+       (1, 4, 1), 
+       (2, 2, 5),   
+       (2, 4, 1),  
+       (3, 1, 2), 
+       (3, 3, 1), 
+       (4, 2, 3),
+       (5, 2, 1), 
+       (5, 4, 2), 
+       (6, 1, 2), 
+       (6, 3, 3);
+	   
+INSERT INTO Pencairan (Rekening_Tujuan, Nominal_Pencairan, Id_StatusPencairan, Id_Metode, Id_Customer)
+VALUES('1234567890', 3000, 1, 1, 1),
+      ('9876543210', 2000, 2, 2, 2),
+      ('5678901234', 5000, 3, 1, 3);
 
 
 
