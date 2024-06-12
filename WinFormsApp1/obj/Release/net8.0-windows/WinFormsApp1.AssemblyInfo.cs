de49a67f2d746c0b3bdef5cbebd38398b2379e4d
BillieSuryaPratama <billie.surya.pratama@gmail.com>
2024-06-12 18:07:20 +0700

    fix

--- WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1572cf8f30f5c6df860c957e460a72fdcb5630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db5a23719641d3c516222e953a44d8c74f7ea300")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
