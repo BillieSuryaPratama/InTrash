c30c993dbae6fa05d9f4c997b785b0f4ae6ec898
EKI JULIO P <ekijulioprasetyo@gmail.com>
2024-06-12 14:27:33 +0700

    Ganti Sandi Nasabah

--- WinFormsApp1/Views/Ganti_Sandi_Nasabah.Designer.cs
+++ WinFormsApp1/Views/Ganti_Sandi_Nasabah.Designer.cs
@@ -28,54 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox4 = new TextBox();
-            textBox3 = new TextBox();
-            textBox2 = new TextBox();
+            konfirmasiSandiTextBox = new TextBox();
+            sandiBaruTextBox = new TextBox();
+            sandiSaatIniTextBox = new TextBox();
             btnKembali = new Button();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            gbKonfirmasi = new GroupBox();
-            label4 = new Label();
-            btnSetuju = new Button();
-            btnBatal = new Button();
             lblInstruksi = new Label();
             btnSimpan = new Button();
-            gbKonfirmasi.SuspendLayout();
             SuspendLayout();
             // 
-            // textBox4
+            // konfirmasiSandiTextBox
             // 
-            textBox4.Location = new Point(279, 189);
-            textBox4.Margin = new Padding(2, 3, 2, 3);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(193, 27);
-            textBox4.TabIndex = 6;
+            konfirmasiSandiTextBox.Location = new Point(349, 236);
+            konfirmasiSandiTextBox.Margin = new Padding(2, 4, 2, 4);
+            konfirmasiSandiTextBox.Name = "konfirmasiSandiTextBox";
+            konfirmasiSandiTextBox.Size = new Size(240, 31);
+            konfirmasiSandiTextBox.TabIndex = 6;
             // 
-            // textBox3
+            // sandiBaruTextBox
             // 
-            textBox3.Location = new Point(279, 143);
-            textBox3.Margin = new Padding(2, 3, 2, 3);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(193, 27);
-            textBox3.TabIndex = 5;
+            sandiBaruTextBox.Location = new Point(349, 179);
+            sandiBaruTextBox.Margin = new Padding(2, 4, 2, 4);
+            sandiBaruTextBox.Name = "sandiBaruTextBox";
+            sandiBaruTextBox.Size = new Size(240, 31);
+            sandiBaruTextBox.TabIndex = 5;
             // 
-            // textBox2
+            // sandiSaatIniTextBox
             // 
-            textBox2.Location = new Point(279, 96);
-            textBox2.Margin = new Padding(2, 3, 2, 3);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(193, 27);
-            textBox2.TabIndex = 4;
+            sandiSaatIniTextBox.Location = new Point(349, 120);
+            sandiSaatIniTextBox.Margin = new Padding(2, 4, 2, 4);
+            sandiSaatIniTextBox.Name = "sandiSaatIniTextBox";
+            sandiSaatIniTextBox.Size = new Size(240, 31);
+            sandiSaatIniTextBox.TabIndex = 4;
             // 
             // btnKembali
             // 
             btnKembali.BackColor = Color.Red;
             btnKembali.ForeColor = Color.White;
-            btnKembali.Location = new Point(11, 309);
-            btnKembali.Margin = new Padding(2, 3, 2, 3);
+            btnKembali.Location = new Point(14, 386);
+            btnKembali.Margin = new Padding(2, 4, 2, 4);
             btnKembali.Name = "btnKembali";
-            btnKembali.Size = new Size(90, 40);
+            btnKembali.Size = new Size(112, 50);
             btnKembali.TabIndex = 7;
             btnKembali.Text = "Kembali";
             btnKembali.UseVisualStyleBackColor = false;
@@ -85,9 +80,10 @@
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.ForeColor = Color.White;
-            label1.Location = new Point(149, 99);
+            label1.Location = new Point(186, 124);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(97, 20);
+            label1.Size = new Size(116, 25);
             label1.TabIndex = 10;
             label1.Text = "Sandi saat ini";
             // 
@@ -96,9 +92,10 @@
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
             label2.ForeColor = Color.White;
-            label2.Location = new Point(149, 145);
+            label2.Location = new Point(186, 181);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(80, 20);
+            label2.Size = new Size(96, 25);
             label2.TabIndex = 11;
             label2.Text = "Sandi Baru";
             // 
@@ -107,60 +104,12 @@
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
             label3.ForeColor = Color.White;
-            label3.Location = new Point(149, 192);
+            label3.Location = new Point(186, 240);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(121, 20);
+            label3.Size = new Size(145, 25);
             label3.TabIndex = 12;
             label3.Text = "Konfirmasi Sandi";
-            // 
-            // gbKonfirmasi
-            // 
-            gbKonfirmasi.BackColor = Color.LightGray;
-            gbKonfirmasi.Controls.Add(label4);
-            gbKonfirmasi.Controls.Add(btnSetuju);
-            gbKonfirmasi.Controls.Add(btnBatal);
-            gbKonfirmasi.ForeColor = Color.Black;
-            gbKonfirmasi.Location = new Point(389, 229);
-            gbKonfirmasi.Margin = new Padding(2, 3, 2, 3);
-            gbKonfirmasi.Name = "gbKonfirmasi";
-            gbKonfirmasi.Padding = new Padding(2, 3, 2, 3);
-            gbKonfirmasi.Size = new Size(240, 120);
-            gbKonfirmasi.TabIndex = 13;
-            gbKonfirmasi.TabStop = false;
-            gbKonfirmasi.Text = "Ubah Sandi";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(30, 33);
-            label4.Name = "label4";
-            label4.Size = new Size(194, 20);
-            label4.TabIndex = 2;
-            label4.Text = "Konfirmasi Perubahan Sandi";
-            // 
-            // btnSetuju
-            // 
-            btnSetuju.BackColor = Color.LimeGreen;
-            btnSetuju.ForeColor = Color.White;
-            btnSetuju.Location = new Point(136, 68);
-            btnSetuju.Margin = new Padding(2, 3, 2, 3);
-            btnSetuju.Name = "btnSetuju";
-            btnSetuju.Size = new Size(90, 39);
-            btnSetuju.TabIndex = 1;
-            btnSetuju.Text = "Setuju";
-            btnSetuju.UseVisualStyleBackColor = false;
-            // 
-            // btnBatal
-            // 
-            btnBatal.BackColor = Color.Red;
-            btnBatal.ForeColor = Color.White;
-            btnBatal.Location = new Point(11, 68);
-            btnBatal.Margin = new Padding(2, 3, 2, 3);
-            btnBatal.Name = "btnBatal";
-            btnBatal.Size = new Size(90, 39);
-            btnBatal.TabIndex = 0;
-            btnBatal.Text = "Batal";
-            btnBatal.UseVisualStyleBackColor = false;
             // 
             // lblInstruksi
             // 
@@ -168,9 +117,10 @@
             lblInstruksi.BackColor = Color.Transparent;
             lblInstruksi.Font = new Font("Segoe UI", 15F);
             lblInstruksi.ForeColor = Color.White;
-            lblInstruksi.Location = new Point(103, 28);
+            lblInstruksi.Location = new Point(129, 35);
+            lblInstruksi.Margin = new Padding(4, 0, 4, 0);
             lblInstruksi.Name = "lblInstruksi";
-            lblInstruksi.Size = new Size(502, 35);
+            lblInstruksi.Size = new Size(600, 41);
             lblInstruksi.TabIndex = 14;
             lblInstruksi.Text = "Masukan kata sandi baru untuk melanjutkan";
             // 
@@ -178,52 +128,45 @@
             // 
             btnSimpan.BackColor = Color.Transparent;
             btnSimpan.BackgroundImage = Properties.Resources.Wireframe___56;
-            btnSimpan.Location = new Point(262, 239);
-            btnSimpan.Margin = new Padding(2, 3, 2, 3);
+            btnSimpan.Location = new Point(328, 299);
+            btnSimpan.Margin = new Padding(2, 4, 2, 4);
             btnSimpan.Name = "btnSimpan";
-            btnSimpan.Size = new Size(90, 44);
+            btnSimpan.Size = new Size(112, 55);
             btnSimpan.TabIndex = 8;
             btnSimpan.Text = "Simpan";
             btnSimpan.UseVisualStyleBackColor = false;
             // 
             // Ganti_Sandi
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.Wireframe___55;
-            ClientSize = new Size(640, 360);
+            ClientSize = new Size(800, 450);
             Controls.Add(lblInstruksi);
-            Controls.Add(gbKonfirmasi);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(btnSimpan);
             Controls.Add(btnKembali);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Margin = new Padding(2, 3, 2, 3);
+            Controls.Add(konfirmasiSandiTextBox);
+            Controls.Add(sandiBaruTextBox);
+            Controls.Add(sandiSaatIniTextBox);
+            Margin = new Padding(2, 4, 2, 4);
             Name = "Ganti_Sandi";
             Text = "InTrash";
-            gbKonfirmasi.ResumeLayout(false);
-            gbKonfirmasi.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private TextBox textBox2;
+        private TextBox konfirmasiSandiTextBox;
+        private TextBox sandiBaruTextBox;
+        private TextBox sandiSaatIniTextBox;
         private Button btnKembali;
         private Label label1;
         private Label label2;
         private Label label3;
-        private GroupBox gbKonfirmasi;
-        private Label label4;
-        private Button btnSetuju;
-        private Button btnBatal;
         private Label lblInstruksi;
         private Button btnSimpan;
     }
--- WinFormsApp1/Views/Ganti_Sandi_Nasabah.cs
+++ WinFormsApp1/Views/Ganti_Sandi_Nasabah.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Npgsql;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinFormsApp1.Controllers;
 
 namespace WinFormsApp1.Views
 {
@@ -15,6 +18,77 @@ namespace WinFormsApp1.Views
         public Ganti_Sandi()
         {
             InitializeComponent();
+
+            btnSimpan.Click += btnSimpan_Click;
+
+        }
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            // Mendapatkan nilai dari TextBox
+            string sandiSaatIni = sandiSaatIniTextBox.Text;
+            string sandiBaru = sandiBaruTextBox.Text;
+            string konfirmasiSandi = konfirmasiSandiTextBox.Text;
+
+            // Memverifikasi bahwa sandi baru dan konfirmasi sandi cocok
+            if (sandiBaru != konfirmasiSandi)
+            {
+                MessageBox.Show("Sandi baru dan konfirmasi sandi tidak cocok.");
+                return;
+            }
+
+            // Memperbarui kata sandi di database
+            UpdatePassword(sandiSaatIni, sandiBaru);
+        }
+
+        private void UpdatePassword(string sandiSaatIni, string sandiBaru)
+        {
+            DBConnection.openConn();
+            string username = Form_Login_Nasabah.UsernameLogin;
+            string queryCheck = "SELECT COUNT(*) FROM Customer WHERE username_customer = @username AND password_customer = @sandiSaatIni";
+            string queryUpdate = "UPDATE Customer SET password_customer = @sandiBaru WHERE username_customer = @username";
+
+            try
+            {
+                {
+                    // Memverifikasi sandi saat ini
+                    using (var cmd = new NpgsqlCommand(queryCheck, DBConnection.connection))
+                    {
+                        cmd.Parameters.AddWithValue("username", username);
+                        cmd.Parameters.AddWithValue("sandiSaatIni", sandiSaatIni);
+
+                        // Menggunakan Convert.ToInt64
+                        long userCount = Convert.ToInt64(cmd.ExecuteScalar());
+                        if (userCount == 0)
+                        {
+                            MessageBox.Show("Sandi saat ini tidak benar.");
+                            return;
+                        }
+                    }
+
+                    // Memperbarui sandi baru
+                    using (var commandUpdate = new NpgsqlCommand(queryUpdate, DBConnection.connection))
+                    {
+                        commandUpdate.Parameters.AddWithValue("username", username);
+                        commandUpdate.Parameters.AddWithValue("sandiBaru", sandiBaru);
+
+                        commandUpdate.ExecuteNonQuery();
+                        MessageBox.Show("Sandi berhasil diperbarui.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+        private void btnKembali_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Profil_Nasabah nextpage = new Profil_Nasabah();
+            nextpage.FormClosed += (s, args) => this.Close();
+            nextpage.ShowDialog();
         }
     }
 }
