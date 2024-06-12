bc44a24b5285d2b9188d0825d1e3c5ff23b14f4e
BillieSuryaPratama <billie.surya.pratama@gmail.com>
2024-06-13 01:10:13 +0700

    Jemput Sampah (DONE)

--- WinFormsApp1/Views/Jemput_Sampah.Designer.cs
+++ WinFormsApp1/Views/Jemput_Sampah.Designer.cs
@@ -29,9 +29,19 @@
         private void InitializeComponent()
         {
             btnKembali = new Button();
-            tbKonfirmasi = new TextBox();
             btnKonfirmasi = new Button();
+            cbKonfirmasi = new ComboBox();
+            btnSelesai = new Button();
+            cbSelesai = new ComboBox();
+            btnTampil1 = new Button();
+            btnTampil2 = new Button();
             dgvListPenjemputan = new DataGridView();
+            id_transaksi = new DataGridViewTextBoxColumn();
+            tanggal_transaksi = new DataGridViewTextBoxColumn();
+            tempat_pengepul = new DataGridViewTextBoxColumn();
+            admin = new DataGridViewTextBoxColumn();
+            customer = new DataGridViewTextBoxColumn();
+            Status_Transaksi = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgvListPenjemputan).BeginInit();
             SuspendLayout();
             // 
@@ -48,14 +58,6 @@
             btnKembali.UseVisualStyleBackColor = false;
             btnKembali.Click += btnKembali_Click;
             // 
-            // tbKonfirmasi
-            // 
-            tbKonfirmasi.Location = new Point(331, 267);
-            tbKonfirmasi.Margin = new Padding(2, 3, 2, 3);
-            tbKonfirmasi.Name = "tbKonfirmasi";
-            tbKonfirmasi.Size = new Size(166, 27);
-            tbKonfirmasi.TabIndex = 38;
-            // 
             // btnKonfirmasi
             // 
             btnKonfirmasi.BackgroundImage = Properties.Resources.Wireframe___56;
@@ -66,17 +68,113 @@
             btnKonfirmasi.TabIndex = 37;
             btnKonfirmasi.Text = "Konfirmasi";
             btnKonfirmasi.UseVisualStyleBackColor = true;
+            btnKonfirmasi.Click += btnKonfirmasi_Click;
+            // 
+            // cbKonfirmasi
+            // 
+            cbKonfirmasi.FormattingEnabled = true;
+            cbKonfirmasi.Location = new Point(463, 269);
+            cbKonfirmasi.Name = "cbKonfirmasi";
+            cbKonfirmasi.Size = new Size(50, 28);
+            cbKonfirmasi.TabIndex = 40;
+            // 
+            // btnSelesai
+            // 
+            btnSelesai.BackgroundImage = Properties.Resources.Wireframe___56;
+            btnSelesai.Location = new Point(518, 313);
+            btnSelesai.Margin = new Padding(2, 3, 2, 3);
+            btnSelesai.Name = "btnSelesai";
+            btnSelesai.Size = new Size(90, 31);
+            btnSelesai.TabIndex = 41;
+            btnSelesai.Text = "Selesai";
+            btnSelesai.UseVisualStyleBackColor = true;
+            btnSelesai.Click += btnSelesai_Click;
+            // 
+            // cbSelesai
+            // 
+            cbSelesai.FormattingEnabled = true;
+            cbSelesai.Location = new Point(463, 315);
+            cbSelesai.Name = "cbSelesai";
+            cbSelesai.Size = new Size(50, 28);
+            cbSelesai.TabIndex = 42;
+            // 
+            // btnTampil1
+            // 
+            btnTampil1.BackgroundImage = Properties.Resources.Wireframe___56;
+            btnTampil1.Location = new Point(189, 267);
+            btnTampil1.Margin = new Padding(2, 3, 2, 3);
+            btnTampil1.Name = "btnTampil1";
+            btnTampil1.Size = new Size(252, 31);
+            btnTampil1.TabIndex = 43;
+            btnTampil1.Text = "Tampilkan 'Diproses'";
+            btnTampil1.UseVisualStyleBackColor = true;
+            btnTampil1.Click += btnTampil1_Click;
+            // 
+            // btnTampil2
+            // 
+            btnTampil2.BackgroundImage = Properties.Resources.Wireframe___56;
+            btnTampil2.Location = new Point(189, 314);
+            btnTampil2.Margin = new Padding(2, 3, 2, 3);
+            btnTampil2.Name = "btnTampil2";
+            btnTampil2.Size = new Size(252, 31);
+            btnTampil2.TabIndex = 44;
+            btnTampil2.Text = "Tampilkan 'Terkonfirmasi'";
+            btnTampil2.UseVisualStyleBackColor = true;
+            btnTampil2.Click += btnTampil2_Click;
             // 
             // dgvListPenjemputan
             // 
             dgvListPenjemputan.BackgroundColor = Color.Gainsboro;
             dgvListPenjemputan.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvListPenjemputan.Location = new Point(33, 20);
-            dgvListPenjemputan.Margin = new Padding(2, 3, 2, 3);
+            dgvListPenjemputan.Columns.AddRange(new DataGridViewColumn[] { id_transaksi, tanggal_transaksi, tempat_pengepul, admin, customer, Status_Transaksi });
+            dgvListPenjemputan.Location = new Point(25, 11);
+            dgvListPenjemputan.Margin = new Padding(2);
             dgvListPenjemputan.Name = "dgvListPenjemputan";
             dgvListPenjemputan.RowHeadersWidth = 62;
-            dgvListPenjemputan.Size = new Size(575, 225);
-            dgvListPenjemputan.TabIndex = 36;
+            dgvListPenjemputan.Size = new Size(583, 228);
+            dgvListPenjemputan.TabIndex = 45;
+            // 
+            // id_transaksi
+            // 
+            id_transaksi.HeaderText = "ID Transaksi";
+            id_transaksi.MinimumWidth = 6;
+            id_transaksi.Name = "id_transaksi";
+            id_transaksi.Width = 125;
+            // 
+            // tanggal_transaksi
+            // 
+            tanggal_transaksi.HeaderText = "Tanggal Transaksi";
+            tanggal_transaksi.MinimumWidth = 6;
+            tanggal_transaksi.Name = "tanggal_transaksi";
+            tanggal_transaksi.Width = 125;
+            // 
+            // tempat_pengepul
+            // 
+            tempat_pengepul.HeaderText = "Tempat Pengepul";
+            tempat_pengepul.MinimumWidth = 6;
+            tempat_pengepul.Name = "tempat_pengepul";
+            tempat_pengepul.Width = 125;
+            // 
+            // admin
+            // 
+            admin.HeaderText = "Admin";
+            admin.MinimumWidth = 6;
+            admin.Name = "admin";
+            admin.Width = 125;
+            // 
+            // customer
+            // 
+            customer.HeaderText = "Customer";
+            customer.MinimumWidth = 6;
+            customer.Name = "customer";
+            customer.Width = 125;
+            // 
+            // Status_Transaksi
+            // 
+            Status_Transaksi.HeaderText = "Status";
+            Status_Transaksi.MinimumWidth = 6;
+            Status_Transaksi.Name = "Status_Transaksi";
+            Status_Transaksi.Width = 125;
             // 
             // Jemput_Sampah
             // 
@@ -84,23 +182,37 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.Wireframe___55;
             ClientSize = new Size(640, 360);
-            Controls.Add(btnKembali);
-            Controls.Add(tbKonfirmasi);
-            Controls.Add(btnKonfirmasi);
             Controls.Add(dgvListPenjemputan);
+            Controls.Add(btnTampil2);
+            Controls.Add(btnTampil1);
+            Controls.Add(cbSelesai);
+            Controls.Add(btnSelesai);
+            Controls.Add(cbKonfirmasi);
+            Controls.Add(btnKembali);
+            Controls.Add(btnKonfirmasi);
             Margin = new Padding(2, 3, 2, 3);
             Name = "Jemput_Sampah";
             Text = "InTrash";
+            Load += Jemput_Sampah_Load;
             ((System.ComponentModel.ISupportInitialize)dgvListPenjemputan).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Button btnKembali;
-        private TextBox tbKonfirmasi;
         private Button btnKonfirmasi;
+        private ComboBox cbKonfirmasi;
+        private Button btnSelesai;
+        private ComboBox cbSelesai;
+        private Button btnTampil1;
+        private Button btnTampil2;
         private DataGridView dgvListPenjemputan;
+        private DataGridViewTextBoxColumn id_transaksi;
+        private DataGridViewTextBoxColumn tanggal_transaksi;
+        private DataGridViewTextBoxColumn tempat_pengepul;
+        private DataGridViewTextBoxColumn admin;
+        private DataGridViewTextBoxColumn customer;
+        private DataGridViewTextBoxColumn Status_Transaksi;
     }
 }
